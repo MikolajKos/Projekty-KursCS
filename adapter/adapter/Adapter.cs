1492469456a4936d7370bde5d5d5f4ce0de77fd9
MikolajKos <95126739+MikolajKos@users.noreply.github.com>
2022-02-19 15:47:48 +0100

    Add files via upload

---
+++ adapter/adapter/Adapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adapter
+{
+    class Adapter : ITarget
+    {
+
+        private readonly Adaptation _adaptt;
+
+        public Adapter(Adaptation adapt)
+        {
+            this._adaptt = adapt;
+        }
+
+        public string GetRequest()
+        {
+            return $"To jest: {this._adaptt.GetSpecificRequest()}";
+        }
+    }
+}
---
+++ adapter/adapter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptation adp = new Adaptation();
+            ITarget target = new Adapter(adp);
+
+            Console.WriteLine("Interface Adaptation jest niekompatybilny z klientem");
+            Console.WriteLine("...ale, za pomocą adaptera klienta moze wywołac właściwą metodę...");
+
+            Console.WriteLine(target.GetRequest());
+
+            Console.ReadKey();
+        }
+    }
+}
